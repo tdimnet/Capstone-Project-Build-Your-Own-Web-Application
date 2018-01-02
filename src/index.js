50cfa8d7922ddef2c516b44cbe7fe60441b251a0
tdimnet <thomas.dimnet@gmail.com>
2018-01-02 18:56:10 +0100

    Start setting up the backend server

---
+++ src/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+console.log('Magic is happening on port ' + port);
