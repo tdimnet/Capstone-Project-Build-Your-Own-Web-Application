3e8fa72b2e10ed2115d9a85d4ab72a00d5ad4839
tdimnet <thomas.dimnet@gmail.com>
2018-01-02 19:10:38 +0100

    Set up the modules needed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
+
+app.use('/', express.static('public'));
+
+// vendor scripts
+app.get(
+  '/vendor/angular.js',
+  (req, res) => {
+    res.sendFile(path.join(__dirname, '../node_modules', 'angular', 'angular.js'));
+  }
+);
+
+app.get(
+  '/vendor/angular-route.js',
+  (req, res) => {
+    res.sendFile(path.join(__dirname, '../node_modules', 'angular-route', 'angular-route.js'));
+  }
+);
 
 app.listen(port);
 console.log('Magic is happening on port ' + port);
