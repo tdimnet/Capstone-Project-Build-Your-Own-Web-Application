db6045c5fb32d49f635e281b1655ca41c055431d
tdimnet <thomas.dimnet@gmail.com>
2018-01-04 18:54:26 +0100

    Fix bug

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ var angular = require('angular');
 angular.module('app', [
   require('angular-route'),
   require('angular-sanitize')
-]).run(function($rootScope, $location, sessionService) {
+]).run(function($rootScope, $location) {
   $rootScope.$on('$routeChangeStart', function(event, next, current) {
     if (next.requireLogin && !sessionService.currentUser.isAuthenthicated) {
       $location.path('/signin');
