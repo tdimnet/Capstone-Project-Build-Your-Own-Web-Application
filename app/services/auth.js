57e40feef4a7d3f18cd1d5fe5174347b741b05b3
tdimnet <thomas.dimnet@gmail.com>
2018-01-16 20:23:58 +0100

    Add the auth service

---
+++ app/services/auth.js
@@ -0,0 +1,61 @@
+'use strict';
+
+function AuthService(sessionService, validationService, dataService, $q) {
+  var _this = this;
+
+  _this.signIn = function(emailAddress, password) {
+    var validationErrors = validationService.getValidationErrorsObject();
+
+    if (!emailAddress) {
+      validationService.addRequiredValidationError(
+        validationErrors,
+        'emailAddress',
+        'Please provide an email address.'
+      )
+    }
+
+    if (!password) {
+      validationService.addRequiredValidationError(
+        validationErrors,
+        'password',
+        'Please provide a password.'
+      )
+    }
+
+    if (validationService.hasValidationErrors(validationErrors)) {
+      return validationService.prepareErrorResponse(validationErrors);
+    }
+
+    var currentUser = sessionService.currentUser;
+    currentUser.emailAddress = emailAddress;
+    currentUser.password = password;
+
+    return dataService
+      .getUser()
+      .then(
+        function(response) {
+          var user = response && response.data && response.data.data && response.data.data[0];
+          currentUser.isAuthenticated = true;
+          currentUser._id = user._id;
+          currentUser.fullName = user.fullName;
+          return $q.resolve(null);
+        },
+        function(response) {
+          sessionService.resetSession();
+          validationService.addVlidationError(
+            validationErrors,
+            'password',
+            validationService.validationCodes.loginFailure,
+            'The login failed for the provided email address and password'
+          );
+          return validationService.prepareErrorResponse(validationErrors);
+        }
+      );
+  }
+
+  _this.signOut = function() {
+    sessionService.resetSession();
+  }
+}
+
+module.exports = AuthService;
--- app/services/index.js
+++ app/services/index.js
@@ -3,5 +3,6 @@
 var angular   = require('angular');
 var app       = angular.module('app');
 
+app.service('authService', require('./auth'));
 app.service('sessionService', require('./session'));
 app.service('validationService', require('./validation'));
