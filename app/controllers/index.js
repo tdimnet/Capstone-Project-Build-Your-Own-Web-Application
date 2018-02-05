ce4bb7a9c32bac2cae4a46bd0d093d11eff74fcb
tdimnet <thomas.dimnet@gmail.com>
2018-02-05 21:30:30 +0100

    Update SignUpController

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var angular = require('angular');
-var app = angular.module('app');
+var angular   = require('angular');
+var app       = angular.module('app');
 
 app.controller('MainController', require('./main-controller'));
 app.controller('SignInController', require('./sign-in'));
