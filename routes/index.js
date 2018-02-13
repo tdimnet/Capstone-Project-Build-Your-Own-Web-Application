2b6ddac3e27c587eee72324e847a1c160f5686bb
tdimnet <thomas.dimnet@gmail.com>
2018-02-13 17:41:41 +0100

    Add middleware

--- routes/index.js
+++ routes/index.js
@@ -2,3 +2,4 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var Restaurant = require('../models/restaurant');
+var mid = require('../middleware');
