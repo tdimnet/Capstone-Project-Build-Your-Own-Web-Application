bffdf4e0ab31e96993b4a538e74321eeb5f3b418
tdimnet <thomas.dimnet@gmail.com>
2018-02-16 17:13:11 +0100

    Update Login logic

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,24 @@ router.get('/login', function(req, res) {
 });
 
 // Post /login
+router.post('/login', function(req, res, next) {
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function(error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
+});
 
 // GET /register
 router.get('/register', function(req, res) {
