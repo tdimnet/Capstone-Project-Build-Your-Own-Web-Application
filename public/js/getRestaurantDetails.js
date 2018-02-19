34789c3dc53579f1cae6bb10de70e653bf168c1d
tdimnet <thomas.dimnet@gmail.com>
2018-02-19 19:22:18 +0100

    Display thematique gif

--- public/js/getRestaurantDetails.js
+++ public/js/getRestaurantDetails.js
@@ -1,34 +1,50 @@
 var restaurantDetails = document.querySelector('#restaurant-details');
 
 function getRestaurantDetails(restaurantId) {
-  var proxyurl = "https://cors-anywhere.herokuapp.com/";
+  var proxyUrl = "https://cors-anywhere.herokuapp.com/";
   var url = `https://api.yelp.com/v3/businesses/${restaurantId}`;
 
   var headers = new Headers();
   headers.append('Authorization', 'Bearer SwVspw3I5otouzM3D6E7pBlhLkQK6lxI01wtE7WUjlu4dcSdXSAXdxAXwIpfPOy1gte76m_94kyWBwjCIYHC4GfKX-iSmmYYkfUKbKndh8_WFm82jT1a7SualN-HWnYx');
 
   return fetch(
-    proxyurl + url,
+    proxyUrl + url,
     {
       method: 'GET',
       headers: headers
     }
   )
     .then(response => response.json())
-    .then(data => displayRestaurantInfo(data))
-    .catch(error => alert(error))
+    .then(data => {
+      getCategoriesHashtags(data.categories)
+      displayRestaurantInfo(data)
+  }).catch(error => alert(error))
+}
+
+function getCategoriesHashtags(data) {
+  var alias = data[0].alias;
+  var proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  var url = `https://api.giphy.com/v1/gifs/search?api_key=Z5WMwvBFVx8W0k3ZsaURnmM9EYFkslC3&q=${alias}&limit=5`;
+  fetch(proxyUrl + url).then(response => response.json()).then(data => displayCategoriesHashtags(data.data))
 }
 
 function displayRestaurantInfo(restaurantData) {
-  console.log(restaurantData)
   var restaurantInfo = `
     <ul>
         <li>${restaurantData.name}</li>
     </ul>
   `;
-
   restaurantDetails.innerHTML = restaurantInfo;
+}
 
+
+function displayCategoriesHashtags(categoriesHashtags) {
+  var categories = '<ul>';
+  for (var i = 0; i < categoriesHashtags.length; i++) {
+    categories += `<li><iframe src="${categoriesHashtags[i].embed_url}" width="400" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></li>`;
+  }
+  categories += '</ul>';
+  restaurantDetails.insertAdjacentHTML('afterend', categories);
 }
 
 window.onload = function() {
